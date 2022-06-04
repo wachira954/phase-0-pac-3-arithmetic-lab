f12bf192e73cfc5ea20529c59586d2ad7959908f
wachira <wachiraa954@gmail.com>
2022-06-05 00:15:23 +0300

    commit tree b85ad92c9b4004a268dbd557f6b703facf4a4a53

---
+++ index.js
@@ -0,0 +1,47 @@
+function multiply() {
+    //value
+}
+
+function add() {
+    //value
+}
+
+function subtract() {
+    //value
+}
+
+function divide() {
+    //value
+}
+
+function add(a, b) {
+    //value
+}
+
+function add(a, b) {
+    return "80";
+}
+
+function subtract(a, b) {
+    return ("b - a")
+}
+
+function multiply(a, b) {
+    return ("number *2 =")
+}
+
+
+
+function divide(a, b) {
+    return ("number /= a, b")
+}
+
+
+let number = 10;
+function add5(a, b) {
+    return ("number += 5")
+}
+
+function divide3(a, b) {
+    return ("number /= 3")
+}
